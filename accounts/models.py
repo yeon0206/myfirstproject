581790eb3960e38e0f2721056037e61210793e14
GIYEON KWON <brucek0206@gmail.com>
2017-11-27 04:19:34 +0900

    profile 모델생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    phone = models.IntegerField(blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+    description = models.CharField(max_length=100, blank=True)
+    photo = models.ImageField(upload_to="accounts/profile/%Y/%m/%d", blank=True)
+    
